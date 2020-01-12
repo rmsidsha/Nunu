9300b44f83ccd0823eff16312db483df97e75ed2
EUNU <rmsidsha@gmail.com>
2020-01-13 02:21:53 +0900

    Gin-gonic example

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	 "github.com/gin-gonic/gin"
-	 "net/http"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong"
+			"message": "pong",
 		})
 	})
 
